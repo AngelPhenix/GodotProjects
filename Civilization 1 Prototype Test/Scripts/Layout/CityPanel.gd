9ba50deeadc4de3115ef849559f20b1f81397357
AngelPhenix <breathstylebro@gmail.com>
2024-10-07 14:00:41 +0200

    Made methods to find available units and buildings to display in city's lists depending on what's been unlocked or not

--- Civilization 1 Prototype Test/Scripts/Layout/CityPanel.gd
+++ Civilization 1 Prototype Test/Scripts/Layout/CityPanel.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+onready var prod_list = get_tree().get_nodes_in_group("production_list")[0]
+
 func open(city_info: Dictionary) -> void:
 	$NamePanel/Name.text = city_info["city_name"]
 	$UnitPanel/Name.text = "Production : " + city_info["unit_in_production"]
@@ -12,4 +14,19 @@ func _on_Exit_pressed():
 
 func _on_Change_pressed():
 	# OPEN PANEL TO CHANGE PRODUCTION UNIT OF THE CITY SELECTED
-	pass # Replace with function body.
+	var available_production = []
+	
+	for unit_name in GlobalData.units_data.keys():
+		if GlobalData.is_unit_unlocked(unit_name):
+			available_production.append(unit_name)
+	for building_name in GlobalData.buildings_data.keys():
+		if GlobalData.is_building_unlocked(building_name):
+			available_production.append(building_name)
+	
+	display_available_production(available_production)
+
+func display_available_production(available_prod: Array):
+	for prod_name in available_prod:
+		var new_option: Button = Button.new()
+		new_option.text = prod_name
+		pass
--- Civilization 1 Prototype Test/Scripts/Singleton/GlobalData.gd
+++ Civilization 1 Prototype Test/Scripts/Singleton/GlobalData.gd
@@ -1,6 +1,48 @@
 extends Node
 
-var units_data = {
-	"Settler": {"attack":0, "hp":1, "movement":2},
-	"Explorer": {"attack":1, "hp":3, "movement":3}
+
+
+
+
+############ UNITS ############
+const units_data = {
+	"Settler": {"attack":0, "hp":1, "movement":2, "production": 5},
+	"Explorer": {"attack":1, "hp":3, "movement":3, "production": 8},
+	"Warrior": {"attack": 3, "hp":5, "movement":2, "production:": 10}
 }
+
+var discovered_units = {
+	"Settler": true,
+	"Explorer": true,
+	"Warrior": true,
+	"Archer": false
+}
+
+func is_unit_unlocked(unit_name: String) -> bool:
+	return discovered_units.get(unit_name, false)
+
+func unlock_unit(unit_name: String) -> void:
+	if units_data.has(unit_name):
+		discovered_units[unit_name] = true
+
+
+
+
+
+############ BUILDINGS ############
+const buildings_data = {
+	"Granary": {"production": 20, "desc": "The Granary gives +2 food."},
+	"School": {"production": 35, "desc": "The School gives +5 science."}
+}
+
+var discovered_buildings = {
+	"Granary": false,
+	"School": false
+}
+
+func is_building_unlocked(building_name: String) -> bool:
+	return discovered_buildings.get(building_name, false)
+
+func unlock_building(building_name: String) -> void:
+	if buildings_data.has(building_name):
+		discovered_buildings[building_name] = true
