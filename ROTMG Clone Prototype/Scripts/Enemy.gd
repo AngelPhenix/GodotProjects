9753b4bbf41ba3671ff8923d81de327eda15c7c1
AngelPhenix <breathstylebro@gmail.com>
2024-10-11 22:05:32 +0200

    Added some possible oscillations to the projectiles if needed

--- ROTMG Clone Prototype/Scripts/Enemy.gd
+++ ROTMG Clone Prototype/Scripts/Enemy.gd
@@ -4,6 +4,10 @@ onready var projectile_scn: PackedScene = preload("res://Scenes/Projectile.tscn"
 
 # FOR THE SHOOT IN WAVE, ALWAYS ADD ONE PROJECTILE UNLESS FULL CIRCLE
 var projectile_count: int = 18
+# Largeur de la wave ????
+var wave_amplitude = 200
+# Hauteur de la wave ????
+var wave_frequency = 4* PI
 
 # Méthode pour tirer en cercle
 func shoot_in_circle() -> void:
@@ -11,7 +15,7 @@ func shoot_in_circle() -> void:
 	for i in range(projectile_count):
 		var angle: float = deg2rad(i * angle_increment) # Tir0 : 0° / Tir1 : 36° / Tir2 : 72° / [...]
 		var direction: Vector2 = Vector2(cos(angle), sin(angle)) # Turns angle into direction Vector
-		spawn_projectile(global_position, direction)
+		spawn_projectile(global_position, direction, true)
 
 # Méthode pour tirer en éventail
 func shoot_in_wave() -> void:
@@ -23,10 +27,13 @@ func shoot_in_wave() -> void:
 		spawn_projectile(global_position, direction)
 
 # Méthode pour créer et tirer un projectile dans une direction donnée
-func spawn_projectile(enemy_pos: Vector2, direction: Vector2) -> void:
+func spawn_projectile(enemy_pos: Vector2, direction: Vector2, wave: bool = false) -> void:
 	var projectile = projectile_scn.instance()
 	projectile.position = enemy_pos
 	projectile.direction = direction
+	if wave:
+		projectile.wave_amplitude = wave_amplitude
+		projectile.wave_frequency = wave_frequency
 	get_parent().add_child(projectile)
 
 
--- ROTMG Clone Prototype/Scripts/Projectile.gd
+++ ROTMG Clone Prototype/Scripts/Projectile.gd
@@ -2,7 +2,41 @@ extends Node2D
 
 var direction: Vector2 = Vector2()
 var speed: int = 200
+var wave_amplitude = 0
+var wave_frequency = 0
+var amplitude_growth = 60
+
+var time_alive = 0
+var lifetime = 15.0
+var initial_delay = 0.5
+
 
 func _process(delta) -> void:
-	# Déplacer le projectile
-	position += direction * speed * delta
+	time_alive += delta
+	
+#	var perpendicular_direction = Vector2(-direction.y, direction.x)
+#
+#	var wave_offset = perpendicular_direction * sin(time_alive * wave_frequency) * (wave_amplitude + amplitude_growth * time_alive)
+#
+#	var move = direction * speed * delta + wave_offset * delta
+#
+#	position += move
+
+	# Faire croître l'amplitude avec le temps
+	var current_amplitude = wave_amplitude + amplitude_growth * time_alive
+	
+	# Calcul du vecteur perpendiculaire à la direction du projectile
+	var perp_dir = Vector2(-direction.y, direction.x)
+	
+	# Mouvement de vague basé sur le temps avec amplitude croissante
+	# Le facteur de temps est multiplié par la fréquence pour contrôler le rythme de l'oscillation
+	var wave_offset = perp_dir * sin(time_alive * wave_frequency) * current_amplitude
+	
+	# Déplacement principal avec un ajout du décalage perpendiculaire
+	var move = direction * speed * delta + wave_offset * delta
+	
+	# Déplacer la balle
+	position += move
+
+	if time_alive >= lifetime:
+		queue_free()
