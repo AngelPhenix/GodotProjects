6cbf9ea5b552b7b1084bfac55a6b20a5c30e1ca2
AngelPhenix <breathstylebro@gmail.com>
2024-10-11 20:33:15 +0200

    Added first shooting method in circle

--- ROTMG Clone Prototype/Scripts/Enemy.gd
+++ ROTMG Clone Prototype/Scripts/Enemy.gd
@@ -2,16 +2,14 @@ extends Node2D
 
 onready var projectile_scn: PackedScene = preload("res://Scenes/Projectile.tscn")
 
-var projectile_count: int = 10
+var projectile_count: int = 36
 
 # Méthode pour tirer en cercle
 func shoot_in_circle() -> void:
-	#36° pour chaque tir
-	# 3*36° => Tir n°3
 	var angle_increment: float = 360 / projectile_count
 	for i in range(projectile_count):
 		var angle: float = deg2rad(i * angle_increment) # Tir0 : 0° / Tir1 : 36° / Tir2 : 72° / [...]
-		var direction: Vector2 = Vector2(cos(angle), sin(angle))
+		var direction: Vector2 = Vector2(cos(angle), sin(angle)) # Turns angle into direction Vector
 		spawn_projectile(global_position, direction)
 
 # Méthode pour créer et tirer un projectile dans une direction donnée
@@ -20,3 +18,7 @@ func spawn_projectile(enemy_pos: Vector2, direction: Vector2) -> void:
 	projectile.position = enemy_pos
 	projectile.direction = direction
 	get_parent().add_child(projectile)
+
+
+func _on_Timer_timeout():
+	shoot_in_circle()
